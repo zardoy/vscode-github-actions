51a4ec1fde4c2a312babe63e70d5d9023815f196
Vitaly Turovsky <vital2580@icloud.com>
2022-10-19 22:27:14 +0300

    add basic fig support methods

--- src/workflow/diagnostics.ts
+++ src/workflow/diagnostics.ts
@@ -3,6 +3,8 @@ import * as vscode from "vscode";
 import { complete, hover, parse } from "github-actions-parser";
 
 import { getGitHubContextForDocumentUri } from "../git/repository";
+import { safeLoad } from 'yaml-ast-parser'
+import { YAMLNode } from "github-actions-parser/dist/types";
 
 const WorkflowSelector = {
   pattern: "**/.github/workflows/*.{yaml,yml}",
@@ -46,6 +48,117 @@ export function init(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.workspace.onDidCloseTextDocument((doc) => collection.delete(doc.uri))
   );
+
+  registerFigSupport()
+}
+
+function getSupportedDocumentAst(document: vscode.TextDocument) {
+  if (!document.fileName.match("(.*)?.github/workflows/(.*).ya?ml")) return
+  return simpleYamlParse(document)
+}
+
+async function registerFigSupport() {
+  const figExtension = vscode.extensions.getExtension('undefined_publisher.fig-unreleased')
+  if (!figExtension) return
+  const api = await figExtension.activate()
+
+  const enablePath = ['jobs', '*', 'steps', 'run']
+  // todo change default cwd to repo root
+  api.registerLanguageSupport(
+    'yaml',
+    {
+        provideSingleLineRangeFromPosition(document: vscode.TextDocument, position: vscode.Position): vscode.Range | undefined {
+          const ast = getSupportedDocumentAst(document)
+          if (!ast) return
+
+          const offset = document.offsetAt(position);
+
+          const {path, node} = findNode(ast, offset)
+          const value = node?.value;
+          if (value && offset >= value.startPosition && offset <= value.endPosition) {
+            const isInRun = enablePath.length === path.length && path.every((val, i) => {
+              const expectedVal = enablePath[i]
+              if (expectedVal === '*') return true
+              return expectedVal === val
+            })
+            if (isInRun) {
+              const ranges = getNodeCommandRanges(document, value)
+              return ranges.find((range) => range.contains(position))
+            }
+          }
+        },
+        getAllSingleLineCommandLocations(document: vscode.TextDocument): vscode.Range[] | undefined {
+          const ast = getSupportedDocumentAst(document)
+          if (!ast) return
+
+          const allMatchingNodes = getAllMatchingNodes(ast, enablePath);
+          return allMatchingNodes.flatMap((valueNode) => getNodeCommandRanges(document, valueNode))
+        }
+    },
+  )
+}
+
+function simpleYamlParse(document: vscode.TextDocument) {
+  return safeLoad(document.getText())
+}
+
+const getNodeCommandRanges = (document: vscode.TextDocument, value: YAMLNode['value']) => {
+  const valStartOffset = document.positionAt(value.startPosition)
+  const valEndOffset = document.positionAt(value.endPosition)
+  const ranges: vscode.Range[] = []
+  const rawText = value.rawValue;
+  if (rawText.startsWith('|')) {
+    const lineMatches = [...rawText.matchAll(/^(\s*)(.+)$/gm)].slice(1)
+    for (const match of lineMatches) {
+      const startOffset = value.startPosition + match.index + match[1].length
+      const endOffset = startOffset + match[2].length
+      ranges.push(new vscode.Range(document.positionAt(startOffset), document.positionAt(endOffset)))
+    }
+  } else {
+    ranges.push(new vscode.Range(valStartOffset, valEndOffset))
+  }
+  return ranges
+}
+
+// much simpler than what lib provides, but ofc I think it should be replaced
+function findNode(input: YAMLNode, position: number) {
+  const path: string[] = []
+  function find(node: YAMLNode): YAMLNode | undefined {
+    if (position >= node.startPosition && position <= node.endPosition) {
+      const key = node.key?.value
+      if (key) path.push(key)
+      const children = node.mappings ?? node.value?.mappings ?? node.value?.items ?? []
+      for (const child of children) {
+        const foundNode = find(child)
+        if (foundNode) return foundNode
+      }
+      return node
+    }
+  }
+  return { node: find(input), path }
+}
+
+const getAllMatchingNodes = (input: YAMLNode, path: string[]) => {
+  const nodes: YAMLNode['value'][] = []
+  const collect = (node: YAMLNode, depth: number): any => {
+    const key = node.key?.value
+    const expectedKey = path[depth]
+    if (key) {
+      if (expectedKey !== '*' && expectedKey !== key) return false
+      if (depth === path.length - 1) {
+        if (node.value) nodes.push(node.value)
+        return
+      }
+    }
+    const nextDepth = key ? depth + 1 : depth
+    const children = node.mappings ?? node.value?.mappings ?? node.value?.items ?? []
+    for (const child of children) {
+      collect(child, nextDepth)
+    }
+  }
+
+  collect(input, 0)
+  return nodes
 }
 
 async function updateDiagnostics(
